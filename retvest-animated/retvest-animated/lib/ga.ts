9ad6d8e5dbf48dfc4b4ca61a3b9f5321f03c9709
kennyfrost123 <81800312+kennyfrost123@users.noreply.github.com>
2025-10-16 18:25:34 +0100

    Create ga.ts

---
+++ retvest-animated/retvest-animated/lib/ga.ts
@@ -0,0 +1,6 @@
+
+--------------------------------------------------
+
+------------------------------------------------```ts
+// Google Analytics 4 – paste your MEASUREMENT_ID here when ready
+export const GA_ID = 'G-XXXXXXXXXX';
